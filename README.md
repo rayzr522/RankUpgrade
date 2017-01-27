f111bd13ab777701afb5782228c10f8572283ff3
Rayzr522 <rayzr522@gmail.com>
2017-01-27 11:00:40 -0500

    Project 'RankUpgrade' generated
    Generated using CNLPluginArchetype and custom CLI project generator tool by Rayzr

---
+++ README.md
@@ -0,0 +1,4 @@
+# RankUpgrade
+A Bukkit plugin for making signs that let you rank up
+
+*Note: This README is a pre-generated stub*
---
+++ src/main/java/com/rayzr522/rankupgrade/RankUpgrade.java
@@ -0,0 +1,15 @@
+package com.rayzr522.rankupgrade;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Rayzr522
+ */
+public class RankUpgrade extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
